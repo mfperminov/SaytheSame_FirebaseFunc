67605f5ba7525aea3d0cdfd94edc532d05303c69
Mikhail Perminov <mfperminov@mail.ru>
2018-08-09 23:20:39 +0400

    debugged

--- functions/index.js
+++ functions/index.js
@@ -5,21 +5,21 @@ admin.initializeApp();
 exports.setInQueue = functions.https.onCall((data, context) => {
   // Grab the text parameter - player Uuid.
   const playerUid = data.text;
-  var rival;
+  var rival, queueCount;
   admin.database().ref('/meta/queue_count').once('value').then(snapshot => {
-    var count;
+    var queueCount = snapshot.val();
+    console.log("queueCount is" + queueCount);
     //if count less then 1 or doesn't exist (empty queue) init it to 1 and
     // write playerUid in queue under path
     // ("/meta/queue_uid/:pushId/{count, player_uid}")
-    if (snapshot.val() == null || snapshot.val() < 1) {
+    if (queueCount == null || queueCount < 1) {
       queueCount = 1;
       writeToQueue(playerUid, queueCount);
     } else {
-      queueCount = snapshot.val();
       //searching for last-in player in queue (his position -
       //current value of queueCount)
       admin.database().ref("/meta").child("queue_uid").orderByChild("count")
-        .equalTo(queueCount).on("value", function(snapshot) {
+        .equalTo(queueCount).once("value", function(snapshot) {
           snapshot.forEach(function(childSnapshot) {
             rivalUid = childSnapshot.child("player_uid").val();
             var currentdate = new Date();
@@ -35,15 +35,22 @@ exports.setInQueue = functions.https.onCall((data, context) => {
               }).then((snap) => {
                 //save game to user2 with same pushId
                 pushKey = snap.key;
-                admin.database().ref('/users/' + playerUid + "/games/" + rivalUid
-                 + "/" + pushKey).set({
-                    uid: rivalUid,
-                    date: currentdate.toString()
-                  });
+                admin.database().ref('/users/' + playerUid + "/games/" + rivalUid +
+                  "/" + pushKey).set({
+                  uid: rivalUid,
+                  date: currentdate.toString()
+                });
               });
-            queueCount = queueCount - 1;
-            admin.database().ref("/meta/queue_uid/" + childSnapshot.key).set(null);
-            admin.database().ref('/meta/queue_count').set(queueCount);
+            //dequeue
+            admin.database().ref("/meta/queue_count")
+            .transaction(function(current_value) {
+              return current_value - 1;
+            });
+            admin.database().ref("/meta/queue_uid/" + childSnapshot.key)
+            .transaction(function(current_value) {
+              return null;
+            });
+            //admin.database().ref('/meta/queue_count').set(queueCount);
           });
         });
     }
@@ -52,7 +59,10 @@ exports.setInQueue = functions.https.onCall((data, context) => {
 });
 
 function writeToQueue(playerUid, count) {
-  admin.database().ref('/meta/queue_count').set(count);
+  admin.database().ref("/meta/queue_count")
+  .transaction(function(current_value) {
+    return (current_value || 0) + 1;
+  });
   admin.database().ref('/meta/queue_uid').push({
     player_uid: playerUid,
     count: count
