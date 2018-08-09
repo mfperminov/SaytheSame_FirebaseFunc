c7018b3b549175123e0910489266fffbc1aec029
Mikhail Perminov <mfperminov@mail.ru>
2018-08-09 20:58:36 +0400

    refactor in queue implementation

--- functions/index.js
+++ functions/index.js
@@ -1,58 +1,56 @@
 const functions = require('firebase-functions');
-
-// The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 admin.initializeApp();
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
+
 exports.setInQueue = functions.https.onCall((data, context) => {
-  // Grab the text parameter.
-  const text = data.text;
+  // Grab the text parameter - player Uuid.
+  const playerUid = data.text;
   var rival;
   admin.database().ref('/meta/queue_count').once('value').then(snapshot => {
     var count;
-    if (snapshot.val() == null) {
-      count = 1;
+    //if count less then 1 or doesn't exist (empty queue) init it to 1 and
+    // write playerUid in queue under path
+    // ("/meta/queue_uid/:pushId/{count, player_uid}")
+    if (snapshot.val() == null || snapshot.val() < 1) {
+      queueCount = 1;
+      writeToQueue(playerUid, queueCount);
     } else {
-      count = snapshot.val();
-      admin.database().ref("/meta").child("queue_uid").orderByChild("count").equalTo(count).on("value", function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-          rival = childSnapshot.child("text").val();
-          var currentdate = new Date();
-          var ref1 = admin.database().ref('/users/' + rival);
-          var ref2 = admin.database().ref('/users/' + text);
-          ref1.once("value").then(function(ref1snap) {
-            if (!ref1snap.hasChild("games")) {
-              admin.database().ref('/users/' + rival).update({
-                games: "games"
+      queueCount = snapshot.val();
+      //searching for last-in player in queue (his position -
+      //current value of queueCount)
+      admin.database().ref("/meta").child("queue_uid").orderByChild("count")
+        .equalTo(queueCount).on("value", function(snapshot) {
+          snapshot.forEach(function(childSnapshot) {
+            rivalUid = childSnapshot.child("player_uid").val();
+            var currentdate = new Date();
+            // for both players create nodes
+            // /users/:user1Uid/games/:user2Uid/:push1Id/{date, result}
+            // /users/:user2Uid/games/:user1Uid/:push2Id/{date, result}
+            // TODO replace uid with result of game (math or unmatch)
+            admin.database().ref('/users/' + rivalUid + "/games/" + playerUid)
+              .push({
+                uid: playerUid,
+                date: currentdate.toString()
               });
-            }
-          });
-          ref2.once("value").then(function(ref2snap) {
-            if (!ref2snap.hasChild("games")) {
-              admin.database().ref('/users/' + text).update({
-                games: "games"
+            admin.database().ref('/users/' + playerUid + "/games/" + rivalUid)
+              .push({
+                uid: rivalUid,
+                date: currentdate.toString()
               });
-            }
-          });
-          admin.database().ref('/users/' + rival + "/games").push({
-            id: text,
-            date: currentdate.toString()
-          });
-          admin.database().ref('/users/' + text + "/games").push({
-            id: rival,
-            date: currentdate.toString()
+            queueCount = queueCount - 1;
+            admin.database().ref("/meta/queue_uid/" + childSnapshot.key).set(null);
+            admin.database().ref('/meta/queue_count').set(queueCount);
           });
         });
-      });
-      count++;
     }
-    admin.database().ref('/meta/queue_count').set(count);
-    admin.database().ref('/meta/queue_uid').push({
-      text: text,
-      count: count
-    });
-
   });
-  return text;
+  return playerUid;
 });
+
+function writeToQueue(playerUid, count) {
+  admin.database().ref('/meta/queue_count').set(count);
+  admin.database().ref('/meta/queue_uid').push({
+    player_uid: playerUid,
+    count: count
+  });
+}
